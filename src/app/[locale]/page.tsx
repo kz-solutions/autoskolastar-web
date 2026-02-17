ff8d223231884141a5c17469fe16afa258184c46
Jan Zdeněk <ydenekjan@gmail.com>
2026-02-17 14:11:15 +0100

    seo | metadata

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,8 +4,41 @@ import StepsToGettingALicense from "@/components/sections/steps-to-getting-a-lic
 import ContactAndLocation from "@/components/sections/contact-and-location/ContactAndLocation";
 import WhyChooseUs from "@/components/sections/why-choose-us/WhyChooseUs";
 import { registerGSAP } from "@/utils/gsap";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
 
 registerGSAP();
+
+type Params = {
+  params: Promise<{ locale: string }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "Metadata" });
+
+  return {
+    icons: {
+      icon: [
+        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      ],
+      apple: "/apple-touch-icon.png",
+      shortcut: "/favicon.ico",
+    },
+    alternates: {
+      languages: {
+        cs: "/cs",
+        en: "/en",
+      },
+    },
+    manifest: "/site.webmanifest",
+    title: t("title"),
+    description: t("description"),
+    keywords: t.raw("keywords"),
+  };
+}
 
 export default function HomePage() {
   return (
